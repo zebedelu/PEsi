8e371023397f68dcfa4d299433eb3312416cf73a
Pedro Vinicius <129136825+zebedelu@users.noreply.github.com>
2025-09-04 21:39:48 -0300

    Add files via upload

--- confirm.js
+++ confirm.js
@@ -1,5 +1,11 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const url = new URL(window.location.href)
-    const emailurl = url.searchParams.get("email")
-    document.getElementById("emailtext").textContent = document.getElementById("emailtext").textContent.replace("emailsuperoficial@iorgute.net",emailurl)
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const url = new URL(window.location.href);
+  const emailurl = url.searchParams.get("email");
+  if (emailurl) {
+    document.getElementById("emailtext").querySelector("strong").textContent =
+      document
+        .getElementById("emailtext")
+        .querySelector("strong")
+        .textContent.replace("emailsuperoficial@iorgute.net", emailurl);
+  }
+});
