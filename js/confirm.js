7e3ff5bde7dfa53758295e6798195ef719ed4d6d
Pedro Vinicius <viniciusppedro6@gmail.com>
2025-09-19 21:51:24 -0300

    analise

--- js/confirm.js
+++ js/confirm.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  Code.addEventListener("input", ()=>{
-    Code.value = Code.value.slice(1, 7)
+  document.getElementById("Code").addEventListener("input", ()=>{
+    document.getElementById("Code").value = document.getElementById("Code").value.slice(1, 7)
   })
   const url = new URL(window.location.href);
   const emailurl = url.searchParams.get("email");
