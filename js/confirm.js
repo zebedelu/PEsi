4ecaad9da69818cfc9295a35c7d18deb3755da59
Pedro Vinicius <viniciusppedro6@gmail.com>
2025-10-01 10:00:23 -0300

    a

--- js/confirm.js
+++ js/confirm.js
@@ -1,14 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("Code").addEventListener("input", ()=>{
-    document.getElementById("Code").value = document.getElementById("Code").value.slice(1, 7)
-  })
-  const url = new URL(window.location.href);
-  const emailurl = url.searchParams.get("email");
-  if (emailurl) {
-    document.getElementById("emailtext").querySelector("strong").textContent =
-      document
-        .getElementById("emailtext")
-        .querySelector("strong")
-        .textContent.replace("emailsuperoficial@iorgute.net", emailurl);
-  }
-});
+document.getElementById("Code").addEventListener("input", ()=>{
+    if (document.getElementById("Code").value.length > 6) {
+        document.getElementById("Code").value = document.getElementById("Code").value.slice(1);
+    }
+})
